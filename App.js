ef7915abed69bc5f0de74c0650cf50cb38fe460e
Nguyen Hai Dang <dangnh@paxcreation.com>
2018-12-11 15:23:29 +0700

    Basic navigation

--- App.js
+++ App.js
@@ -6,8 +6,9 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator, createAppContainer, createBottomTabNavigator } from 'react-navigation';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,16 +17,108 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'Home',
+  };
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+        <Button title="Go to details" onPress={() => this.props.navigation.navigate("Details")} />
       </View>
     );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam('otherParam', 'A Nested Details Screen')
+    }
+  };
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+            otherParam: "This Is Real Details"
+          })}
+        />
+        <Button
+          title="Pop to top"
+          onPress={() => this.props.navigation.popToTop()}
+        />
+        <Button
+          title="Update the title"
+          onPress={() => this.props.navigation.setParams({ otherParam: 'Updated!' })}
+        />
+      </View>
+    );
+  }
+}
+
+class SettingsScreen extends Component {
+  static navigationOptions = {
+    title: 'Settings',
+  };
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Settings Screen</Text>
+        <Button title="Go to profile" onPress={() => this.props.navigation.navigate("Profile")} />
+      </View>
+    );
+  }
+}
+
+class ProfileScreen extends Component {
+  static navigationOptions = {
+    title: 'Profile',
+  };
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Profile Screen</Text>
+      </View>
+    );
+  }
+}
+
+const StackNavigator1 = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Details: {
+    screen: DetailsScreen
+  }
+});
+
+const StackNavigator2 = createStackNavigator({
+  Settings: {
+    screen: SettingsScreen
+  },
+  Profile: {
+    screen: ProfileScreen
+  }
+});
+
+const TabNavigator = createBottomTabNavigator({
+  Tab1: StackNavigator1,
+  Tab2: StackNavigator2
+});
+
+const AppContainer = createAppContainer(TabNavigator);
+
+export default class App extends Component {
+  handleNavigationChange = (prevState, newState, action) => {
+    console.log("handleNavigationChange", prevState, newState, action);
+  }
+  render() {
+    return <AppContainer onNavigationStateChange={this.handleNavigationChange} />
   }
 }
 
